7dd2c844cb79656b3481323d89453de7bf0a2839
paras231 <sharmaparas553@gmail.com>
2025-01-22 00:06:24 +0530

    updated payment service as a consumer , and user service as producer

--- user-service/index.js
+++ user-service/index.js
@@ -8,13 +8,14 @@ const app = express();
 
 app.use(express.json());
 
+connectWithRabbitmq();
 
 let channel , connection;
 
 
 async function connectWithRabbitmq(){
     try {
-        connection = await amqp.connect("amp://localhost");
+        connection = await amqp.connect("amqp://localhost");
         // Create a channel
     channel = await connection.createChannel();
     channel.assertQueue("send_message");
@@ -24,15 +25,31 @@ async function connectWithRabbitmq(){
     }
 }
 
-async function sendMessageNotification(payload){
+ async function sendSignupNotification  (message)  {
     try {
-        
+      if (!channel) {
+        console.error("RabbitMQ channel is not available");
+        return;
+      }
+  
+      // Send message to the queue
+      channel.sendToQueue(
+        "signup_notifications",
+        Buffer.from(JSON.stringify(message))
+      );
+      console.log(`Message sent to queue: ${JSON.stringify(message)}`);
     } catch (error) {
-        
+      console.error("Error sending message:", error);
     }
-}
+  };
+  
 
 app.post("/send-message", async(req,res)=>{
+    await sendSignupNotification({
+        userName:"Paras",
+        age:24,
+        timeStamp:Date.now()
+    });
     res.status(200).json("success");
 })
 
